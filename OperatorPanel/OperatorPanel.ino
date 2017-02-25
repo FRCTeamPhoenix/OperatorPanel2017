7f1a83b928416e4f161f31a83745c55e4e6ccd4a
Jeff Brockway <jeff@brockway.com>
2017-02-25 16:09:24 -0500

    second version

--- OperatorPanel/OperatorPanel.ino
+++ OperatorPanel/OperatorPanel.ino
@@ -6,25 +6,49 @@ Joystick_ Joystick;
 
 int FlyWheel = 2;
 int LEDoutput = 13;
+int Indexer = 3;
+int Shooter = 4;
+int ClimberUp = 5;
+int ClimberDown = 6;
+int AutonomousStart = 7;
+int AutonomousStop = 8;
+int PotX = A0;
+int PotY = A1;
+int FlyWheelSpeed = A2;
+int Turret = A3;
+
+volatile int *lastFlyWheelState;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(FlyWheel, INPUT);
   pinMode(LEDoutput, OUTPUT);
-  pinMode(A0, INPUT);
-
+  pinMode(Indexer, INPUT);
+  pinMode(Shooter, INPUT);
+  pinMode(ClimberUp, INPUT);
+  pinMode(ClimberDown, INPUT);
+  pinMode(AutonomousStart, INPUT);
+  pinMode(AutonomousStop, INPUT);
+  pinMode(PotX, INPUT);
+  pinMode(PotY, INPUT);
+  pinMode(FlyWheelSpeed, INPUT);
+  pinMode(Turret, INPUT);
+  
+  //attachInterrupt(FLyWheel )
+  
   // Initialize Joystick Library
   Joystick.begin();
   Serial.begin(9600);
 
 }
-// Last state of the flyWheel
-int lastFlyWheelState = 0;
+
+
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int currentFlyWheelState = !digitalRead(FlyWheel);
+  readButtonState(FlyWheel, 0, lastFlyWheelState);
+  
   int KnobValue = analogRead(A0);
   Serial.print(KnobValue);
   Serial.print('\n');
@@ -32,16 +56,20 @@ void loop() {
   Joystick.setYAxis(KnobValue);
   Joystick.setZAxis(KnobValue);
   Joystick.setRxAxis(KnobValue);
-  
-  
-  if (currentFlyWheelState != lastFlyWheelState){
-    Joystick.setButton(0, currentFlyWheelState);
-    lastFlyWheelState = currentFlyWheelState;
-    Serial.print("Button Press \n");
-    }
-    digitalWrite(LEDoutput, currentFlyWheelState); 
-    delay(50);
     
+  }
+
+ void readButtonState(int pin, int button, int *lastState) {
+  int currentState = !digitalRead(pin);
+  if (currentState != *lastState){
+    Joystick.setButton(button, currentState);
+    *lastState = currentState;
+    Serial.print("Button Press ");
+    Serial.print(button);
+    Serial.print(" ");
+    Serial.print(currentState);
+    Serial.print('\n');
+    }
     
   }
 
