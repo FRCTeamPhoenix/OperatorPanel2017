b1072b1ccf0af1107c41fec315ca491fb427f032
Jeff Brockway <jeff@brockway.com>
2017-03-05 22:30:23 -0500

    Ben added code to display the flywheel manual speed percentage on the seven segment serial display.

--- OperatorPanel/OperatorPanel.ino
+++ OperatorPanel/OperatorPanel.ino
@@ -1,4 +1,5 @@
 #include <Joystick.h>
+#include <SoftwareSerial.h>
 
 //Written by Maya Yaakov 2/25/16
 //This is for the Operator Control panel used by team 2342 
@@ -35,6 +36,10 @@ int TurretRotation = A5;
 int FlywheelSpeed = A2;
 
 // TODO: Need to add Serial 7-segment Rx pin
+const int softwareTx = 12;
+const int softwareRx = 13;
+SoftwareSerial perDis(softwareRx, softwareTx);
+char tempString[10]; // Will be used with sprintf to create strings
 
 // TODO: Need to add LED output pins
 int ledFlywheelTrim = 0;
@@ -81,6 +86,30 @@ void setup() {
   pinMode(FlywheelSpeed, INPUT);
 
   // TODO: Need to add Serial 7-segment Rx pin
+  perDis.begin(9600);
+
+  perDis.write(0x76);  // Clear display command
+  perDis.write(0x7A);  // Set brightness command byte
+  perDis.write(200);  // brightness data byte
+
+  perDis.print("0000");
+  delay(1000);
+  perDis.print("   -");
+  delay(250);
+  perDis.print("  --");
+  delay(250);
+  perDis.print(" ---");
+  delay(250);
+  perDis.print("----");
+  delay(250);
+  perDis.print("--- ");
+  delay(250);
+  perDis.print("--  ");
+  delay(250);
+  perDis.print("-   ");
+  delay(250);
+  perDis.print("    ");
+  delay(250);
 
   // TODO: Need to add LED output pins
   pinMode(ledFlywheelTrim, OUTPUT);
@@ -90,14 +119,14 @@ void setup() {
   
   
   // Future possible interrupt method
-  //attachInterrupt(FLyWheel )
+  //attachInterrupt(FlyWheel )
   
   // Initialize Joystick Library
   Joystick.begin();
 
   // Initialize serial for seven-segment display
   // TODO set the pins
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
 }
 
@@ -129,11 +158,13 @@ void loop() {
   if (currentState != lastState){
     Joystick.setButton(button, currentState);
     lastState = currentState;
+    /*
     Serial.print("Button Press "); 
     Serial.print(button); 
     Serial.print(" ");
     Serial.print(currentState); 
     Serial.print('\n');
+    */
     }
     
   }
@@ -202,7 +233,7 @@ void loop() {
        else{
         analogWrite(ledTurretRotation, 0);
        }
-       
+       /*
        Serial.print("Pot ");
         Serial.print(axis);
         Serial.print(" ");
@@ -210,6 +241,7 @@ void loop() {
         Serial.print(" ");
         Serial.print(lastState);
         Serial.print('\n');
+        */
         
       }
     else if(axis == 4){
@@ -217,6 +249,11 @@ void loop() {
        //currentValue = currentValue/1023.0;
        
        Joystick.setThrottle(currentValue);
+       int potValue = 1023 - currentValue;
+       int scaledValue = (int) ((float) potValue/10.23);
+       sprintf(tempString, "%3d ", (int) scaledValue);
+       perDis.print(tempString);
+       delay(100);
       }
 
     
